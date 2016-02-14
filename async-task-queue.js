bacb26ab898aa13f0d9c229cb8b36dbef406dd25
talsu <talsu84@gmail.com>
2016-02-14 22:13:39 +0900

    dequeueAndExecute method to be private.

--- async-task-queue.js
+++ async-task-queue.js
@@ -1,44 +1,51 @@
 (function () {
   "use strict";
 
-  this.AsyncTaskQueue = (function () {
-    function AsyncTaskQueue() {
-      var self = this;
+  var AsyncTaskQueue = function () { return new TaskQueue(); };
+
+  // export for node js
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = AsyncTaskQueue;
+    }
+    exports.AsyncTaskQueue = AsyncTaskQueue;
+  } else {
+    this.AsyncTaskQueue = AsyncTaskQueue;
+  }
+
+  var TaskQueue = (function () {
+    function TaskQueue() {
       this.queue = [];
       this.isExecuting = false;
-
-      // Dequeue and execute job.
-      this.dequeueAndExecute = function () {
-        if (self.isExecuting) return;
-
-        // Dequeue Job.
-        var job = self.queue.shift();
-        if (!job) return;
-
-        // next job execute function.
-        function next(){
-          self.isExecuting = false;
-          self.dequeueAndExecute();
-        }
-
-        //Execute Job.
-        self.isExecuting = true;
-
-        // Pass next job execute callback.
-        job(next);
-      };
     }
 
     // enqueue job. run immediately.
-    AsyncTaskQueue.prototype.enqueue = function (job) {
+    TaskQueue.prototype.enqueue = function (job) {
       // enqueue.
       this.queue.push(job);
       // call execute.
-      this.dequeueAndExecute();
+      dequeueAndExecute(this);
     };
 
-    return AsyncTaskQueue;
-  })();
+    // Dequeue and execute job.
+    function dequeueAndExecute (self) {
+      if (self.isExecuting) return;
 
+      // Dequeue Job.
+      var job = self.queue.shift();
+      if (!job) return;
+
+      //Execute Job.
+      self.isExecuting = true;
+
+      // Pass next job execute callback.
+      job(function() {
+        self.isExecuting = false;
+        dequeueAndExecute(self);
+      });
+    }
+
+    return TaskQueue;
+  })();
 
 }.call(this));
